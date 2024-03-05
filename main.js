7efbc46917951e138dd9d2402005f2fed426bd13
cerebrus6 <diokristofferyap.dky@gmail.com>
2024-03-05 20:26:11 +0800

    enhc: remove redundant files and code

--- main.js
+++ main.js
@@ -4,18 +4,7 @@ import Core from 'cerebrus6core';
 // const Core = require('@core');
 
 // Modules
-import * as rng from "./cerebrus6/rng.js";
-import * as safeBet from "./cerebrus6/safeBet.js";
-import * as meme from "./cerebrus6/meme.js";
-import * as person from "./cerebrus6/person.js";
-import * as cat from "./cerebrus6/cat.js";
-import * as waifu from "./cerebrus6/waifu.js";
-import * as headline from "./cerebrus6/headline.js";
 import * as todo from "./cerebrus6/todo.js";
-import * as tini from "./cerebrus6/tini.js";
-import * as fantasy from "./cerebrus6/fantasy.js";
-import * as snack from "./cerebrus6/snack.js";
-import * as joke from "./cerebrus6/joke.js";
 
 class Bot extends Core {
   constructor() {
@@ -23,18 +12,7 @@ class Bot extends Core {
 
     this.client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
     this.modules = {
-      rng: rng,
-      safeBet: safeBet,
-      meme: meme,
-      person: person,
-      cat: cat,
-      waifu: waifu,
-      headline: headline,
       todo: todo,
-      tini: tini,
-      fantasy: fantasy,
-      snack: snack,
-      joke: joke,
       help: this.help
     };
   }
